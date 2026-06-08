645a3244248ca588a570e201ff419583a2d6fa1d
yufo211 <95125392+yufo211@users.noreply.github.com>
2026-06-08 15:51:38 +0900

    Add prettier.lua

---
+++ prettier.lua
@@ -0,0 +1,13 @@
+require("conform").setup({
+	formatters_by_ft = {
+		css = { "prettierd", "prettier", stop_after_first = true },
+		javascript = { "prettierd", "prettier", stop_after_first = true },
+		javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+		json = { "prettierd", "prettier", stop_after_first = true },
+		html = { "prettierd", "prettier", stop_after_first = true },
+		markdown = { "prettierd", "prettier", stop_after_first = true },
+		typescript = { "prettierd", "prettier", stop_after_first = true },
+		typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+		yaml = { "prettierd", "prettier", stop_after_first = true },
+	},
+})
