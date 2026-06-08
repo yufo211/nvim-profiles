6e18c3d4161a667d0b9ed817b7351982498295b5
yufo211 <95125392+yufo211@users.noreply.github.com>
2026-06-08 15:55:23 +0900

    Update prettier.lua

--- prettier.lua
+++ prettier.lua
@@ -1,5 +1,6 @@
 require("conform").setup({
 	formatters_by_ft = {
+		astro = { "prettierd", "prettier", stop_after_first = true },
 		css = { "prettierd", "prettier", stop_after_first = true },
 		javascript = { "prettierd", "prettier", stop_after_first = true },
 		javascriptreact = { "prettierd", "prettier", stop_after_first = true },
@@ -8,6 +9,7 @@ require("conform").setup({
 		markdown = { "prettierd", "prettier", stop_after_first = true },
 		typescript = { "prettierd", "prettier", stop_after_first = true },
 		typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+		vue = { "prettierd", "prettier", stop_after_first = true },
 		yaml = { "prettierd", "prettier", stop_after_first = true },
 	},
 })
